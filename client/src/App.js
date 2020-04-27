7af40a2c96cffe555deb67e6da48bc075029fc02
Mimi <mimi@MacBook-Air-Oj-11.local>
2020-04-27 11:26:17 +0200

    Resolving git conflictss

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,4 @@ import React from 'react';
 import './App.css';
 import MealPlanner from './Component/MealPlanner';
 
-export default function App() {
-  return (
-    <div>
-      <MealPlanner />
-    </div>
-  );
-}
+
--- routes/index.js
+++ routes/index.js
@@ -6,48 +6,5 @@ router.get('/', function (req, res, next) {
   res.send('index', { title: 'Express' });
 });
 
-const apiKey = process.env.apiKey;
-
-//RECIPE INSTRUCTIONS
-router.get('/recipe/:id', function (req, res) {
-  fetch(`https://api.spoonacular.com/recipes/id=${req.params.id}/information?apiKey=${apiKey}`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-    })
-    .catch(err => res.send(err))
-});
-
-//RECIPE GENERATOR
-router.get('/recipe/:ingredients', function (req, res) {
-  fetch(`https://api.spoonacular.com/recipes/findByIngredients=${req.params.findByIngredients}ingredients=&apiKey=${apiKey}`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-    })
-    .catch(err => res.send(err))
-});
-
-// SHOPPING LIST
-router.get('/recipe/:id/ingredient', function (req, res) {
-  fetch(`https://api.spoonacular.com/recipes/id=${req.params.id}/ingredientWidget?apiKey=${apiKey}`)
-    .then(res => res.text())
-    .then(data => {
-      console.log(data)
-    })
-    .catch(err => res.send(err))
-});
-
-//MEAL PLANNER
-router.get('/recipe/search/:diet/:excludeIngredients/:intolerances/:cuisine', function (req, res) {
-  fetch(`https://api.spoonacular.com/recipes/search?diet=${req.params.diet}&excludeIngredients${req.params.excludeIngredients}=&intolerances=${req.params.intolerances}&cuisine=${req.params.cuisine}&number=21&apiKey=${apiKey}`)
-    .then(res => res.json())
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => res.send(err))
-});
-
-
 
 module.exports = router;
