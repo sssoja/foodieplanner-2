eaaae9858607a6c883c7dd370153fb1e763325d8
sophie <sophieleila@icloud.com>
2020-04-29 20:46:25 +0200

    create recipe instructions view with dummy data

--- client/src/components/RecipeInstructions.js
+++ client/src/components/RecipeInstructions.js
@@ -5,15 +5,17 @@ import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import { BrowserRouter as Router, Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
 // import CardHeader from '@material-ui/core/CardHeader';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import CardContent from '@material-ui/core/CardContent';
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
 
 class RecipeInstructions extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-     recipe: []
+      recipe: [],
     };
   }
 
@@ -25,16 +27,74 @@ class RecipeInstructions extends React.Component {
       });
   }
 
+  useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+    media: {
+      height: 0,
+      paddingTop: "56.25%", // 16:9
+    },
+  });
+
   render() {
+    const classes = makeStyles();
     return (
-      <div>
+      <div className={classes.root}>
         <Container>
-          <Paper>
-            <Typography variant="h4">Recipe</Typography>
-            <Grid>
-              <Card></Card>
+          <Grid container spacing={3}>
+            <Grid item xs={6}>
+              <br />
+              <Paper>
+                <Box p={3}>
+                  <Typography variant="h4">
+                    {this.state.recipe.title}
+                  </Typography>
+                  <hr />
+                  <div>
+                    <Box p={2}>
+                      <Card>
+                        <CardContent>
+                          <Typography
+                            variant="h6"
+                            style={{ color: "rgb(33, 48, 55)" }}
+                            gutterBottom
+                          >
+                            Ready in minutes: {this.state.recipe.readyInMinutes}
+                          </Typography>
+                          <Typography
+                            variant="h6"
+                            style={{ color: "rgb(33, 48, 55)" }}
+                          >
+                            Serves: {this.state.recipe.servings}
+                          </Typography>
+                          <br></br>
+                          <CardMedia className={classes.media} />{" "}
+                          <img
+                            src={
+                              "https://spoonacular.com/recipeImages/" +
+                              this.state.recipe.id +
+                              "-480x360.jpg"
+                            }
+                          />
+                          <br></br>
+                          <br></br>
+                          <Typography>{this.state.recipe.summary}</Typography>
+                          <br></br>
+                        </CardContent>
+                      </Card>
+                    </Box>
+                  </div>
+                </Box>
+              </Paper>
             </Grid>
-          </Paper>
+          </Grid>
         </Container>
       </div>
     );
