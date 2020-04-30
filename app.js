999c3674eacaaa8817813c142634c92166fa1a68
doublebyte1 <doublebyte@gmail.com>
2020-04-30 17:39:23 +0200

    - added changes to support build on heroku

--- app.js
+++ app.js
@@ -39,4 +39,12 @@ app.use(function (err, req, res, next) {
   res.send('error');
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 module.exports = app;
