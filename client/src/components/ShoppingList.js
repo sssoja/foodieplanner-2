ab263f8d029ca96379282b299eb394ad7278009c
Laura Font <laurafontsalvador@gmail.com>
2020-05-02 23:23:32 +0200

    shopping list almost working

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 
 export default class ShoppingList extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            loading: true,
             recipes: this.props.location.state.recipesPlanner,
             ingredients: [],
-            ingredientsName: [],
-            ingredientsImage: []
+            ingredientsArray: [],
+            ingredientsObject: {
+                name: "",
+                image: "",
+                amount: {
+                    value: 0,
+                    unit: ""
+                }
+            },
+            updatedIngredientsArray: []
         }
     };
 
@@ -24,41 +34,86 @@ export default class ShoppingList extends Component {
             let json = await response.json();
             this.setState({ ingredients: [...this.state.ingredients, json]});     
         }
-        await this.fetchIngredientsName();
-        await this.fetchIngredientsImage();
+        await this.createIngredientsObject();
+       // await this.generateShoppingList();
+        await this.removeDuplicates();
+        await this.setState({loading: false});
     }
 
-    fetchIngredientsName() {
+    createIngredientsObject() {
         for(let i=0; i<this.state.ingredients.length; i++) {
             let array = this.state.ingredients[i].ingredients
              for(let j=0; j<array.length; j++){
-                this.setState({ ingredientsName: [...this.state.ingredientsName, array[j].name]});
+                let ingredientsObject = {...this.state.ingredientsObject};
+                ingredientsObject.name = array[j].name;
+                ingredientsObject.image = array[j].image;
+                ingredientsObject.amount = array[j].amount.metric;               
+                this.setState({ingredientsObject});
+                this.setState({ ingredientsArray: [...this.state.ingredientsArray, this.state.ingredientsObject]});
             }
+            
         };
     }
 
-    fetchIngredientsImage() {
-        for(let i=0; i<this.state.ingredients.length; i++) {
-            let array = this.state.ingredients[i].ingredients
-             for(let j=0; j<array.length; j++){
-                this.setState({ ingredientsImage: [...this.state.ingredientsImage, array[j].image]});
+/*        generateShoppingList() {
+        const array = this.state.ingredientsArray;
+        for(let i=0; i<array.length; i++) {
+            for(let j=0; j<array.length; j++) {
+                if(i !== j) {
+                    if(array[i].name !== array[j].name) {
+                        this.setState({ updatedIngredientsArray: [...this.state.updatedIngredientsArray, array[i]]});
+                        console.log(this.state.updatedIngredientsArray)
+                    } //name is not repeated
+                    //push to updatedIngredientsArray
+                    
+                   // this.setState({ updatedIngredientsArray: [...this.state.updatedIngredientsArray, array[i]]});
+                    } else {
+                    //add amounts
+                    array[i].amount.value += array[j].amount.value
+                    //push to updatedIngredientsArray
+                    this.setState({ updatedIngredientsArray: [...this.state.updatedIngredientsArray, array[i]]});
+                }        
+                
             }
-        };
-    }
+            
+        } */
 
-    
+        removeDuplicates() {
+            let lookup = {};
+            let result = [];
+            const array = this.state.ingredientsArray;
+            for(let i=0; i<array.length; i++) {
+                if(!lookup[array[i].name]){
+                    lookup[array[i].name] = true;
+                    result.push(array[i]);
+                }
+            }
+            this.setState({ updatedIngredientsArray: result});
+        }
+
+/*         removeDuplicates() {
+            const array = this.state.ingredientsArray;
+            const newArray = array.filter((value, index) => array.indexOf(value) === index);
+            this.setState({ updatedIngredientsArray: newArray});
+        } */
+  
 
     render() {
         return (
             <div>
+                <div>
+                {this.state.loading ? <CircularProgress /> : "Page Loaded"}
+                </div>
                 <Box p={3}>
                     <Typography variant="h4">Shopping List</Typography>
                 </Box>
                 <Box p={3}>
-                    {this.state.ingredientsImage.map((ingredient, index) => {
+                    {this.state.updatedIngredientsArray.map((ingredient, index) => {
                       return (
                       <div key={index}>
-                            <img src={"https://spoonacular.com/cdn/ingredients_100x100/" + ingredient} />
+                            <h4>{ingredient.name}</h4>
+                            <img src={"https://spoonacular.com/cdn/ingredients_100x100/" + ingredient.image} />
+                            <p>{ingredient.amount.value} {ingredient.amount.unit}</p>
                       </div>
                       );
                     })}
